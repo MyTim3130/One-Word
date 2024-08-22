d04603251f1cc096a34fdf526e6e433dacc83bf3
MyTim3130 <tin.hauslkramper@gmail.com>
2024-08-22 18:53:19 +0200

    gpt integration fix

--- frontend/app/app/game/[id]/voting/page.jsx
+++ frontend/app/app/game/[id]/voting/page.jsx
@@ -5,52 +5,61 @@ import useUserStore from '@/app/_store/user.store'
 import {socket} from '@/app/variables'
 
 const gptObject = {
-  "words": [
-      "Übermorgen",
-      "unternehme",
-      "ich",
-      "nichts",
-      ", deshalb",
-      "unternehme",
-      "ich",
-      "übermorgen",
-      "nichts."
-  ],
-  "players": [
-      {
-          "id": "NBj4q2o48QY4fT6DAAAr",
-          "name": "Timo",
-          "host": true,
-          "words": [
-              "Übermorgen",
-              "ich",
-              ", deshalb",
-              "ich",
-              "nichts."
-          ],
-          "points": 100,
-          "place": 2
-      },
-      {
-          "id": "8WRpyB8AuWQcHXH8AAAp",
-          "name": "Tim",
-          "host": false,
-          "words": [
-              "unternehme",
-              "nichts",
-              "unternehme",
-              "übermorgen"
-          ],
-          "points": 450,
-          "place": 1
-      }
-  ]
+  "gpt": {
+      "words": [
+          "Viele",
+          "Egypter",
+          "inhalieren",
+          "vapes",
+          "weswegen",
+          "ihre",
+          "Mächte",
+          "schwinden."
+      ],
+      "players": [
+          {
+              "id": "FQCvSbP7FOI41U0GAAAv",
+              "name": "Tim",
+              "host": true,
+              "words": [
+                  "Viele",
+                  "vapes",
+                  "Mächte"
+              ],
+              "points": 350,
+              "place": 1
+          },
+          {
+              "id": "CALqV-Fs5g_VAo1oAAAz",
+              "name": "Timo",
+              "host": false,
+              "words": [
+                  "Egypter",
+                  "weswegen",
+                  "schwinden."
+              ],
+              "points": 300,
+              "place": 2
+          },
+          {
+              "id": "hTBraO74JfRCNPMuAAA5",
+              "name": "Alex",
+              "host": false,
+              "words": [
+                  "inhalieren",
+                  "ihre"
+              ],
+              "points": 150,
+              "place": 3
+          }
+      ]
+  }
 }
 
 const Voting = () => {
   const {players, setPlayers} = usePlayersStore()
   const {user, setUser} = useUserStore()
-  const [votingData, setVotingData] = useState(gptObject)
+  const [votingData, setVotingData] = useState(null)
 
 useEffect(() => {
  socket.emit("getVotingData",)
@@ -67,29 +76,27 @@ useEffect(() => {
     <main className='flex justify-between items-center flex-col'>
       <h1 className='text-3xl p-10'>Leaderboard</h1>
     
+      {votingData === null ? (
+        <div className='text-2xl'>Loading...</div>
+      ) : (
         <div className='flex w-fit justify-around p-20 gap-36 bg-[#95D5B2] rounded-xl'>
-
-       
-        {votingData.players.map((player) => (
-          <section key={player.id} className='flex flex-col justify-between'>
-            <div>
-          <div>
-            <h2 className='text-xl mb-3'>{player.name}</h2>
-            <div>
-              {votingData.players.find((p) => p.id === player.id).words.map((word, index) => (
-                <div key={index}>- {word}</div>
-              ))}
-             
-            </div>
-           
-          </div>
-          </div>
-           <div className='text-lg font-medium mt-5'>Points: {player.points}</div>
-           </section>
-        ))}
- 
-      </div>
-
+          {votingData.gpt.players.map((player) => (
+            <section key={player.id} className='flex flex-col justify-between'>
+              <div>
+                <div>
+                  <h2 className='text-xl mb-3'>{player.name}</h2>
+                  <div>
+                    {votingData.gpt.players.find((p) => p.id === player.id).words.map((word, index) => (
+                      <div key={index}>- {word}</div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className='text-lg font-medium mt-5'>Points: {player.points}</div>
+            </section>
+          ))} 
+        </div>
+      )}
     </main>
   )
 }
