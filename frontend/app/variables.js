8b283f7fffb0715c8a53861cd0c429af50814f0d
MyTim3130 <tin.hauslkramper@gmail.com>
2024-08-26 12:15:16 +0200

    server link

--- frontend/app/variables.js
+++ frontend/app/variables.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-export const socket = io("ws://localhost:3002");
-//export const socket = io("wss://oneword.ball-2000.com");
+//export const socket = io("ws://localhost:3002");
+export const socket = io("wss://oneword.ball-2000.com");
