a132a68553da505d3276d911146265b9f3774568
MyTim3130 <tin.hauslkramper@gmail.com>
2025-08-02 14:59:36 +0200

    domain

--- frontend/app/socket/socket.js
+++ frontend/app/socket/socket.js
@@ -3,5 +3,5 @@
 import { io } from "socket.io-client";
 
 
-export const socket = io("85.215.231.160:3002", {
+export const socket = io("onewordbackend.timhausl.com", {
   transports: ["websocket"]});
